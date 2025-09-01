363cec3cebd4db03c8d87c61711ba04a0d0f2cf1
Itor Carlos <itor51carlos@gmail.com>
2025-09-01 20:23:12 -0300

    primeiro commit frontend

---
+++ frontend/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Image
+        src="/path/to/image.jpg"
+        alt="Description of image"
+        width={500}
+        height={500}
+      />
+    </div>
+  );
+}
