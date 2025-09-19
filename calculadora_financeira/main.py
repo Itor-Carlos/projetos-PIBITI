84709c5b5feae36cb36c3332d6731d906f49bfe7
Itor Carlos <itor51carlos@gmail.com>
2025-09-19 09:45:36 -0300

    refactor

--- calculadora_financeira/main.py
+++ calculadora_financeira/main.py
@@ -2,9 +2,11 @@ import mysql.connector
 from enum import Enum
 from mcp.server.fastmcp import FastMCP
 from fastapi import FastAPI
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 mcp = FastMCP("calculadora_financeira")
-# app = FastAPI()
 
 class Tipo(Enum):
     RECEITA = "Receita"
@@ -42,21 +44,18 @@ def get_connection():
     )
 
 
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 @mcp.tool()
-async def inserir_transacao(tipo: Tipo, categoria: Categoria, valor: float, descricao: str, data: str):
+async def inserir_transacao(type: Tipo, category: Categoria, amount: float, description: str, date: str):
     try:
         """
         Inserting a financial transaction into the database.
         Args:
-            tipo: Tipo da transação (Receita ou Despesa)
-            categoria: Categoria da transação (e.g. Alimentação, Transporte)
-            valor: Valor da transação
-            descricao: Descrição da transação
-            data: Data da transação (YYYY-MM-DD)
+            type: Transaction type (Income or Expense)
+            category: Transaction category (e.g. Food, Transportation)
+            amount: Transaction amount
+            description: Transaction description
+            date: Transaction date (YYYY-MM-DD)
         Returns:
             A dict indicating success or failure.
         """
@@ -64,7 +63,7 @@ async def inserir_transacao(tipo: Tipo, categoria: Categoria, valor: float, desc
         connection = get_connection()
         cursor = connection.cursor()
         sql = "INSERT INTO transacao (tipo, categoria, valor, descricao, data) VALUES (%s, %s, %s, %s, %s)"
-        values = (tipo.value, categoria.value, valor, descricao, data)
+        values = (type.value, category.value, amount, description, date)
         cursor.execute(sql, values)
         connection.commit()
         cursor.close()
@@ -124,22 +123,6 @@ async def transacao_mes_ano(mes: int, ano: int):
 
 from enum import Enum
 
-class Categoria(Enum):
-    ALIMENTACAO = "Alimentação"
-    TRANSPORTE = "Transporte"
-    MORADIA = "Moradia"
-    LAZER = "Lazer"
-    SAUDE = "Saúde"
-    EDUCACAO = "Educação"
-    OUTROS = "Outros"
-
-def get_categoria(valor: str):
-    for cat in Categoria:
-        if cat.value == valor:
-            return cat
-    return None
-
-
 @mcp.tool()
 async def get_resume_by_categoria(categoria: str):
     """
@@ -176,7 +159,6 @@ async def get_resume_by_categoria(categoria: str):
     except Exception as exception:
         return {"status": "erro", "detalhe": str(exception)}
 
-
 @mcp.tool()
 async def get_transacoes(date: str):
     """
@@ -204,4 +186,3 @@ async def get_transacoes(date: str):
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
-    #app.run()
